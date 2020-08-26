6eb92cefe6bb6bc22a03c57031c562b1a3715634
Alessandro Di Tecco <alessandro@ditecco.me>
2020-08-27 01:06:46 +0200

    Removed unneeded file, moved script

--- static/admin/netlify-id.js
+++ static/admin/netlify-id.js
@@ -1,13 +1,3 @@
 /* ---------------------------------
 https://www.netlifycms.org/docs/add-to-your-site/
 --------------------------------- */
-
-if (window.netlifyIdentity) {
-  window.netlifyIdentity.on("init", user => {
-    if (!user) {
-      window.netlifyIdentity.on("login", () => {
-        document.location.href = "/admin/"
-      })
-    }
-  })
-}
