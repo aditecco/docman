3b03959eec93aa578a49d7d7659c5fa517335438
Alessandro Di Tecco <alessandro@ditecco.me>
2020-08-27 03:26:13 +0200

    CMS fixes & fine-tuning

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -2,21 +2,22 @@
 cms.js
 --------------------------------- */
 
-import CMS from "netlify-cms-app"
-import DocTemplate from "../templates/doc.template"
+// TODO create a custom template w/o props, or use registerPreviewStyle
+// import CMS from "netlify-cms-app"
+// import DocTemplate from "../templates/doc.template"
 
-CMS.registerPreviewTemplate("docs", DocTemplate)
+// CMS.registerPreviewTemplate("docs", DocTemplate)
 
 // https://www.netlifycms.org/docs/add-to-your-site/
-// if (window.netlifyIdentity) {
-//   window.netlifyIdentity.on("init", user => {
-//     if (!user) {
-//       window.netlifyIdentity.on("login", () => {
-//         log("YO")
-//         document.location.href = "/admin/"
-//       })
-//     }
-//   })
-// }
+if (window.netlifyIdentity) {
+  window.netlifyIdentity.on("init", user => {
+    if (!user) {
+      window.netlifyIdentity.on("login", () => {
+        log("YO")
+        document.location.href = "/admin/"
+      })
+    }
+  })
+}
 
-console.log("@@@@@@@@@@cms.js")
+console.log("@@@ cms.js")
