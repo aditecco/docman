187d70050b1feb4603188cf1cb169e310af99331
Alessandro Di Tecco <alessandro@ditecco.me>
2020-09-22 00:54:28 +0200

    Created index page boilerplate

---
+++ src/pages/docs/index.tsx
@@ -0,0 +1,11 @@
+/* ---------------------------------
+Index
+--------------------------------- */
+
+import React, { ReactElement } from "react"
+
+interface IOwnProps {}
+
+export default function Index(props: IOwnProps): ReactElement {
+  return <div>Index</div>
+}
