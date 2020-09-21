c3527d071a062399042a230d8be587f7ded2ff57
Alessandro Di Tecco <alessandro@ditecco.me>
2020-09-22 00:51:41 +0200

    Refactored config w/ multiple instances of `gatsby-source-filesystem`

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,44 +10,6 @@ module.exports = {
     `gatsby-plugin-sharp`,
     `gatsby-plugin-sass`,
     {
-      resolve: `gatsby-plugin-netlify-cms`,
-      options: {
-        modulePath: `${__dirname}/src/cms/cms.js`,
-        publicPath: `admin`,
-        htmlTitle: `DocMan Admin`,
-      },
-    },
-    // {
-    //   resolve: "gatsby-plugin-load-script",
-    //   options: {
-    //     src: ``,
-    //   },
-    // },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options:
-        // options: {
-        //   name: `images`,
-        //   path: `${__dirname}/src/images`,
-        // },
-        {
-          name: "markdown-pages",
-          path: `${__dirname}/content/docs`,
-        },
-    },
-    // {
-    //   resolve: `gatsby-plugin-manifest`,
-    //   options: {
-    //     name: `gatsby-starter-default`,
-    //     short_name: `starter`,
-    //     start_url: `/`,
-    //     background_color: `#663399`,
-    //     theme_color: `#663399`,
-    //     display: `minimal-ui`,
-    //     icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-    //   },
-    // },
-    {
       resolve: `gatsby-transformer-remark`,
       options: {
         // CommonMark mode (default: true)
@@ -99,6 +61,53 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: "cookbooks",
+        path: `${__dirname}/src/content/cookbooks`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: "docs",
+        path: `${__dirname}/src/content/docs`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: "quick-refs",
+        path: `${__dirname}/src/content/quick-refs`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-netlify-cms`,
+      options: {
+        modulePath: `${__dirname}/src/cms/cms.js`,
+        publicPath: `admin`,
+        htmlTitle: `DocMan Admin`,
+      },
+    },
+    // {
+    //   resolve: "gatsby-plugin-load-script",
+    //   options: {
+    //     src: ``,
+    //   },
+    // },
+    // {
+    //   resolve: `gatsby-plugin-manifest`,
+    //   options: {
+    //     name: `gatsby-starter-default`,
+    //     short_name: `starter`,
+    //     start_url: `/`,
+    //     background_color: `#663399`,
+    //     theme_color: `#663399`,
+    //     display: `minimal-ui`,
+    //     icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+    //   },
+    // },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
