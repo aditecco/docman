355d84bbbd1e823b7e8f0e9f47ef3e9bec462fa9
Alessandro Di Tecco <alessandro@ditecco.me>
2022-01-09 03:20:17 +0100

    Enabled TOC plugin, moved constants to own file

--- src/utils/index.js
+++ src/utils/index.js
@@ -8,69 +8,9 @@ import remarkRehype from "remark-rehype"
 import rehypeSanitize, { defaultSchema } from "rehype-sanitize"
 import rehypeStringify from "rehype-stringify"
 import rehypePrism from "@mapbox/rehype-prism"
+import remarkToc from "remark-toc"
+import { ALLOWED_CLASSES, ALLOWED_TOKENS, TOC_KEY } from "../constants"
 // import remarkGfm from "remark-gfm"
-// import remarkToc from "remark-toc"
-
-const ALLOWED_CLASSES = [
-  "className",
-  "language-js",
-  "language-javascript",
-  "language-css",
-  "language-md",
-  "language-scss",
-  "language-mdx",
-  "language-ts",
-  "language-tsx",
-  "language-jsx",
-  "language-bash",
-  "language-php",
-  "language-pug",
-  "language-text",
-]
-
-const ALLOWED_TOKENS = [
-  "className",
-  "addition",
-  "attr",
-  "attribute",
-  "built_in",
-  "bullet",
-  "char",
-  "code",
-  "comment",
-  "deletion",
-  "doctag",
-  "emphasis",
-  "formula",
-  "keyword",
-  "link",
-  "literal",
-  "meta",
-  "name",
-  "number",
-  "operator",
-  "params",
-  "property",
-  "punctuation",
-  "quote",
-  "regexp",
-  "section",
-  "selector-attr",
-  "selector-class",
-  "selector-id",
-  "selector-pseudo",
-  "selector-tag",
-  "string",
-  "strong",
-  "subst",
-  "symbol",
-  "tag",
-  "template-tag",
-  "template-variable",
-  "title",
-  "type",
-  "variable",
-]
 
 export async function parseWithRemark(source) {
   if (!source) {
@@ -79,6 +19,7 @@ export async function parseWithRemark(source) {
 
   return await unified()
     .use(remarkParse)
+    .use(remarkToc, { heading: TOC_KEY })
     .use(remarkRehype)
     .use(rehypeSanitize, {
       // https://github.com/rehypejs/rehype-sanitize#example-syntax-highlighting
